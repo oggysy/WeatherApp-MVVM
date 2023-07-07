81025db6425b9ff02d21ff450790e156b337e734
oggysy <ogisoys@gmail.com>
2023-07-07 11:57:59 +0900

    ボタンタップの処理をIBActionからRxSwiftに変更

--- WeatherApp-MVVM/Views/Detail/DetailViewController.swift
+++ WeatherApp-MVVM/Views/Detail/DetailViewController.swift
@@ -6,20 +6,23 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class DetailViewController: UIViewController {
 
     
     @IBOutlet weak var detailTableView: UITableView!
+    @IBOutlet weak var closeButton: UIButton!
+    let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         detailTableView.register(UINib(nibName: "DetailTableViewCell", bundle: nil), forCellReuseIdentifier: "DetailTableViewCell")
         detailTableView.dataSource = self
-    }
-    
-    @IBAction func closedButtonTapped(_ sender: Any) {
-        self.dismiss(animated: true)
+        closeButton.rx.tap.subscribe { _ in
+            self.dismiss(animated: true)
+        }.disposed(by: disposeBag)
     }
 }
 
