b17768f1e77b25aed922a6a322f2cd924f0ddedb
oggysy <ogisoys@gmail.com>
2023-07-31 21:34:49 +0900

    重複した処理を削除

--- WeatherApp-MVVM/Views/Detail/DetailViewController.swift
+++ WeatherApp-MVVM/Views/Detail/DetailViewController.swift
@@ -60,11 +60,6 @@ class DetailViewController: UIViewController {
             viewModel.isLoadingDriver.drive(loadingView.indicator.rx.isAnimating),
             viewModel.isLoadingDriver.map { !($0) }.drive(loadingView.rx.isHidden),
             viewModel.isLoadingDriver.map { !($0) }.drive(view.rx.isUserInteractionEnabled),
-            viewModel.isLoading
-                .bind(to: loadingView.indicator.rx.isAnimating),
-            viewModel.isLoading
-                        .map { !($0) }
-                        .bind(to: loadingView.rx.isHidden),
             viewModel.APIErrorMessageDriver.drive(onNext: { message in
                 let alert = UIAlertController(title: "エラー", message: message, preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "閉じる", style: .default, handler: { _ in
